08f55062081a1a1ecd8eb1e8165a04a35ac03e44
Muhammad Rizki Amrullah <amrizakisi@gmail.com>
2017-03-04 22:18:03 +0700

    Created by Coding Ground Client

---
+++ Pretest Pemahaman Java/Tugas1.java
@@ -0,0 +1,8 @@
+public class Tugas1{
+
+     public static void main(String []args){
+        String nama = "Rizki Amrullah";
+        String kota = "Jepara";
+        System.out.println("Nama Saya = "+nama+" , Kota = "+kota);
+     }
+}
