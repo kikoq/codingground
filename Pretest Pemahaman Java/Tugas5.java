9f7fa397df38bf81d543c3107c522d540b6971dc
Muhammad Rizki Amrullah <amrizakisi@gmail.com>
2017-03-04 22:18:55 +0700

    Created by Coding Ground Client

---
+++ Pretest Pemahaman Java/Tugas5.java
@@ -0,0 +1,19 @@
+public class Tugas5{
+    
+    
+    public static void main(String []args){
+        
+        HitungMundur(10);
+    
+    }
+    
+    public static void HitungMundur(int angka){
+        
+        for(int i = angka; i>0; i--){
+            System.out.println(i);
+        }
+    }
+    
+    
+    
+}
