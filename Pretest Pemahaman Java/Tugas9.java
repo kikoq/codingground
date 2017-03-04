f1b9f8e9cb1df15f7adc4a90af408ad2a0c88788
Muhammad Rizki Amrullah <amrizakisi@gmail.com>
2017-03-04 22:21:00 +0700

    Created by Coding Ground Client

---
+++ Pretest Pemahaman Java/Tugas9.java
@@ -0,0 +1,17 @@
+public class Tugas9{
+    
+    String nama;
+    
+    public Tugas9(String nama){
+        
+        this.nama = nama;
+    }
+        
+        Tugas9 data = new Tugas9("Rizki Amrullah");
+        
+    public static void main(String []args){
+    
+        
+    }
+    
+}
